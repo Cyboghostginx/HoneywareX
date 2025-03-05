34b3ddae85c943d26254e409a2638d42dc612b63
Cyboghostginx <146610955+Cyboghostginx@users.noreply.github.com>
2025-03-05 18:29:34 +0000

    Update ai_server.sh

--- ai_server.sh
+++ ai_server.sh
@@ -1,17 +1,13 @@
 #! /bin/bash
 
-# Models we can test in this project, many more in Ollama library (Open-Source)
+# Models To be tested in this project (Open-Source)
 # -
-# TinyLlama - 1.1B (ollama pull tinyllama) - ❌
-# Gemma - 2B (ollama pull gemma:2b) - ✅
-# Llama 3.2 - 3B (ollama pull 3.2:3b) - ✅
-# Qwen 2.5 - 3B (ollama pull qwen2.5:3b) - ✅
+# Llama 3.2 - 3B (ollama pull 3.2:3b) - 
+# Qwen 2.5 - 3B (ollama pull qwen2.5:3b) - 
 # -
 # Dolphin3 - 8B (ollama pull dolphin3) - 
-# Llama3 - 8B (ollama pull llama3) - 
+# Llama3.1 - 8B (ollama run llama3.1) - 
 # Gemma - 9B (ollama pull gemma2:latest) - 
-# Zephyr - 7B (ollama pull zephyr) - 
-# Phi-4 - 14b (ollama pull phi4) - 
 
 
 # This script can be used to setup our AI server equiped with the baseline models needed, and also with an NGROK tunneling that allows us to use this model on any other project.
@@ -23,7 +19,7 @@ sleep 3
 echo -e "\n"
 
 # Pull Ollama's models needed from repository
-ollama pull llama3.2:3b # any Model to be used can be changed here
+ollama pull llama3.1 # any Model to be used can be changed here
 sleep 3
 # We can start with these 2 model for testing and add on later.
 
@@ -42,4 +38,4 @@ curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
 ngrok config add-authtoken 2qLsPVOpFTwyh61s6XD2aJh1iRT_6LBBmpHsJ5PUmrqJnQ4Ag # This is a just free NGROK token, nothing personal (:
 
 # Get Ngrok Link from port 11434 that Ollama usually runs on
-ngrok http 11434 --host-header="localhost:11434" --log=stdout >/dev/null & sleep 3 && echo -e "\n" && curl -s http://localhost:4040/api/tunnels | jq -r '.tunnels[0].public_url'
+ngrok http 11434 --host-header="localhost:11434" --log=stdout >/dev/null & sleep 3 && echo -e "\n" && echo "You can use this link for your Ollama model's inference anywhere in the world" && curl -s http://localhost:4040/api/tunnels | jq -r '.tunnels[0].public_url'
